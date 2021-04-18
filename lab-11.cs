8a9c93ae7c65afbb5cd7115b3a32808de9839a49
kirany39 <69450066+kirany39@users.noreply.github.com>
2021-04-18 19:31:28 +0530

    Update lab-11.cs

--- lab-11.cs
+++ lab-11.cs
@@ -9,7 +9,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace WindowsFormsApplication9
+namespace WindowsFormsApplication3
 {
     public partial class Form1 : Form
     {
